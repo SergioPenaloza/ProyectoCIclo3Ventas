ed56d44d8d58eb47db2517a0de5c1aa133b42425
LEFAVEI89 <leonardo.vergara.pmaa@gmail.com>
2021-10-22 22:22:41 -0500

    Login-React-auth0

---
+++ Login-react-auth0/src/components/Links.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Links = () => (
+  <nav>
+    <Link to="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">Home</Link>
+  </nav>
+ );
+
+export default Links;
